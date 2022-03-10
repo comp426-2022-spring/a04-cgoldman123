1e9e73f49748b8d14a9d5214fb242fc9220c519a
cgoldman123 <cgoldman@live.unc.edu>
2022-03-10 16:26:04 -0500

    initial commit for a4

---
+++ database.js
@@ -0,0 +1,18 @@
+const Database = require('sqlite3')
+
+const db = new Database('log.db')
+
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and 'access'`)
+let row = stmt.get()
+
+if (row === undefined) {
+    console.log('log database is missing. Creating log database.')
+    const sqlInit = `
+        CREATE TABLE accesslog ( remoteaddr NUMERIC PRIMARY KEY, remoteuser TEXT, time NUMERIC, method VARCHAR, url NUMERIC, protocol TEXT, httpversion NUMERIC, secure NUMERIC, status INTEGER, referer NUMERIC, useragent NUMERIC)
+        `
+    logdb.exec(sqlInit)
+} else {
+    console.log('Log database exists')
+}
+
+module.exports = db
