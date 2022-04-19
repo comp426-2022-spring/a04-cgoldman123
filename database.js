3a1d49d95bb89db243de86b027f097b86f429149
cgoldman123 <cgoldman@live.unc.edu>
2022-04-19 18:46:22 -0400

    hopefully fixed db

--- database.js
+++ database.js
@@ -9,7 +9,7 @@ let row =  stmt.get();
 if (row === undefined) {
     console.log('log database is missing. Creating log database.')
     const sqlInit = `
-        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT remoteuser TEXT, time TEXT, method TEXT, 1 url TEXT, protocol TEXT, httpversion TEXT, status TEXT, referer TEXT, useragent TEXT);
+        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status TEXT, referer TEXT, useragent TEXT );
         `
     db.exec(sqlInit)
     
