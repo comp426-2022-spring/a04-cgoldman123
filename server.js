fc8ec4510af96005556e8c8bf9aabc824307af23
cgoldman123 <cgoldman@live.unc.edu>
2022-04-19 19:25:27 -0400

    fuck

--- server.js
+++ server.js
@@ -1,34 +1,47 @@
-const args = require('minimist')(process.argv.slice(2))
-// Define allowed argument name 'help'.
-if (args.help === true) {
-  console.log('(--help).*(Return this message and exit.)')
-  process.exit(0)
-}
-
 const fs = require('fs')
 const morgan = require('morgan')
 
 const express = require('express')
 
 const app = express()
+const args = require('minimist')(process.argv.slice(2))
 
-// const cors = require('cors');
-// app.use(cors())
+// If --help, echo help text and exit
+if (args.help || args.h) {
+    console.log(`
+    server.js [options]
+    --port, -p	Set the port number for the server to listen on. Must be an integer
+                between 1 and 65535.
+    --debug, -d If set to true, creates endlpoints /app/log/access/ which returns
+                a JSON access log from the database and /app/error which throws 
+                an error with the message "Error test successful." Defaults to 
+                false.
+    --log		If set to false, no log files are written. Defaults to true.
+                Logs are always written to database.
+    --help, -h	Return this message and exit.
+    `)
+    process.exit(0)
+}
 
-
-var db = require("./database.js")
-app.use(express.urlencoded({ extended: true}));
+const db = require('./database.js')
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-if (args.log == "false") {
-    console.log("not creating it")
-} else{
+const port = args.port || args.p || 5555
+// Start server
+const server = app.listen(port, () => {
+    console.log(`App is running on port ${port}`)
+});
+
+if (args.log == 'false') {
+    console.log("I'm not creating shit here")
+
+} else {
     const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
     app.use(morgan('combined', { stream: accessLog }))
 }
 
-// Always log to database
 app.use((req, res, next) => {
     let logdata = {
         remoteaddr: req.ip,
@@ -44,51 +57,35 @@ app.use((req, res, next) => {
     };
     // console.log(logdata)
     const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
-    // const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
-    // console.log(info)
+    //const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
+    //console.log(info)
     next();
 })
-
-
-
-// Define allowed argument name 'port'.
-
-// Define a const `port` using the argument from the command line. 
-// Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args.port || process.env.PORT || 5555
-
-
-// let logging = morgan('combined')
-// app.use(logging('common'))
-
-
-const server = app.listen(port, () => {
-    console.log(`App is running on port ${port}`)
-});
-
-// look at express.js example online
-app.get('/app', (req, res)  => {
+app.get("/app", (req, res, next) => {
     res.json({"message":"Your API works! (200)"});
-    res.status(200);
+	res.status(200);
 });
-
 
 if (args.debug || args.d) {
-    app.get("app/log/access",(req, res) => {
-        const stmt = db.prepare('SELECT * FROM accesslog').all()
-        res.status(200).json(stmt);
-        // perhaps wanna try res.status(200).json(logData)
+    app.get('/app/log/access', (req, res, next) => {
+        const stmt = db.prepare("SELECT * FROM accesslog").all();
+	    res.status(200).json(stmt);
     })
-    app.get("app/error", (req, res) => {
-        // res.status(500).send("500 Internal Server Error")
-        throw new Error('Error test works');
-    }) 
+
+    app.get('/app/error/', (req, res, next) => {
+        throw new Error('Error test works.')
+    })
 }
 
-app.use(function(req, res) {
-    res.status(404).send("404 NOT FOUND")
-    res.type("text/plain") 
-    
-})
+app.use(function(req, res){
+    const statusCode = 404
+    const statusMessage = 'NOT FOUND'
+    res.status(statusCode).end(statusCode+ ' ' +statusMessage)
+});
 
+// Tell STDOUT that the server is stopped
+// process.on('SIGINT', () => {
+ //   server.close(() => {
+//		console.log('\nApp stopped.');
+//	});
+//});
