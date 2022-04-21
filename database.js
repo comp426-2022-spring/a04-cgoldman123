73c9611a12ae376fbb8d50596ba1538e3e05ff05
cgoldman123 <cgoldman@live.unc.edu>
2022-04-21 12:16:08 -0400

    hope this works

--- database.js
+++ database.js
@@ -5,11 +5,11 @@ const db = new Database('log.db')
 
 const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog'`)
 let row =  stmt.get();
-// testing autograder 
+// this checks if the database exists and creates one if it does not
 if (row === undefined) {
     console.log('log database is missing. Creating log database.')
     const sqlInit = `
-        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status TEXT, referrer TEXT, useragent TEXT );
+        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, url TEXT, protocol TEXT, httpversion TEXT, status TEXT, referrer TEXT, useragent TEXT );
         `
     db.exec(sqlInit)
     
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-const port = args.port || args.p || 5555
+const port = args.port ||  5555
 
 const server = app.listen(port, () => {
     console.log(`App is running on port ${port}`)
@@ -60,8 +60,7 @@ app.use((req, res, next) => {
     next();
 })
 app.get("/app", (req, res, next) => {
-    res.json({"message":"Your API works! (200)"});
-	res.status(200);
+	res.status(200).send("Your API works! (200)");
 });
 
 if (args.debug || args.d) {
@@ -79,3 +78,89 @@ app.use(function(req, res) {
     res.status(404).send("404 NOT FOUND")
     res.type("text/plain")  
 })
+
+
+
+function coinFlips(flips) {
+    var coinArray = new Array()
+    for (let i=0; i < flips; i++) {
+      coinArray[i] = coinFlip()
+    }
+  
+    return coinArray 
+  }
+  
+  function coinFlip() {
+      let randomNum = Math.random()
+      if (randomNum < .5){
+        return "tails"
+      } else{
+        return "heads"
+      }
+    }
+  
+    
+  
+    function countFlips(myArray) {
+      var headCount = 0
+      var tailCount = 0
+      for (let i=0; i<myArray.length; i++) {
+        if (myArray[i] == 'heads') {
+          headCount = headCount + 1
+        } else {
+          tailCount = tailCount + 1
+        }
+      }
+      /*if (headCount ==0) {
+        return "{ tails: " + tailCount + " }"
+      }
+      if (tailCount ==0) {
+        return "{ heads: " + headCount + " }"
+      }*/
+      let results = {
+        tails: tailCount,
+        heads: headCount
+      }
+      return results
+    }
+  
+
+  
+  app.get('/app/flip', (req, res)  => {
+      res.status(200).json({'flip' : coinFlip()})
+  })
+  
+  // need to return game as from screenshot
+  app.get('/app/flip/call/heads', (req, res)  => {
+    var flip = coinFlip()
+    if (flip == "heads") {
+      result = "win"
+    } else {
+      result = "lose"
+    }
+    res.status(200).json({'call':'heads', 'flip':flip, 'result':result})
+  })
+  // test
+  app.get('/app/flip/call/tails', (req, res)  => {
+    var flip = coinFlip()
+    if (flip == "tails") {
+      result = "win"
+    } else {
+      result = "lose"
+    }
+    res.status(200).json({'call':'tails', 'flip':flip, 'result':result})
+  })
+  // making sure I committed right
+  
+  
+  
+  
+  app.get('/app/echo/:repeat', (req, res) => {
+      res.status(200).json({'message': req.params.repeat})
+  })
+  
+  app.get('/app/flips/:number', (req, res) => {
+    var coinArray = coinFlips(req.params.number)
+    var mySummary = countFlips(coinArray)
+    res.status(200).json({'raw': coinArray, "summary": mySummary})
+  })
